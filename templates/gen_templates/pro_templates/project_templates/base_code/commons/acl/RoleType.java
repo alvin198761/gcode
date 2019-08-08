96d26845c95c4a5f8aa0b9139a03f3581b74632e
月球王子 <alvin198761@163.com>
2019-08-08 10:11:59 +0800

    重新提交一下

--- templates/gen_templates/pro_templates/project_templates/base_code/commons/acl/RoleType.java
+++
@@ -1,8 +0,0 @@
-package ${project.base_package}.common.acl;
-
-public enum RoleType {
-
-    ADMIN //超级管理员
-    ,
-    EMPLOYEE //子账号
-}
--- templates/gen_templates/pro_templates/project_templates/base_code/commons/exception/ErrorCode.java
+++
@@ -1,7 +0,0 @@
-package ${project.base_package}.common.exception;
-
-public interface ErrorCode {
-
-	int EGENERALEXCEPTION = 500;   // rdesc
-
-}
